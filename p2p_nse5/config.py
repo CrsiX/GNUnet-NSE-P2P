cab3f2661fda37be45db4502757bf7a0e2f47a4b
Chris <chris@hopfen.space>
2022-06-07 15:34:59 +0200

    Added a config loader module

---
+++ p2p_nse5/config.py
@@ -0,0 +1,37 @@
+"""
+Configuration parser module
+"""
+
+import os
+import configparser
+
+DEFAULT_SECTION = "_default"
+GLOBAL_SECTION = "_global"
+
+
+def load_configuration(filenames: list[str]) -> dict[str, dict[str, str]]:
+    """
+    Load a dictionary-based configuration from a list of INI-style config files
+
+    In case the configuration file contains top-level entries without prior
+    section, those entries are listed below a global section :var:`GLOBAL_SECTION`.
+
+    :param filenames: list of filenames to search for
+    :return: dictionary of sections to key-value pairs
+    """
+
+    try:
+        config = configparser.ConfigParser(default_section=DEFAULT_SECTION)
+        config.read(filenames)
+        return {k: dict(v) for k, v in config.items() if k != DEFAULT_SECTION}
+    except configparser.MissingSectionHeaderError:
+        for filename in filenames:
+            if not os.path.exists(filename):
+                continue
+            with open(filename) as f:
+                content = f.read()
+            config = configparser.ConfigParser(default_section=DEFAULT_SECTION)
+            config.read_string(f"[{GLOBAL_SECTION}]{os.linesep}{content}", filename)
+            return {k: dict(v) for k, v in config.items() if k != DEFAULT_SECTION}
+        else:
+            raise RuntimeError("No configuration file found")
