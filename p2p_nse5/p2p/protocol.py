fc78fbbad09c3080e550b0389b9bd179ebaff9d4
Chris <chris@hopfen.space>
2022-07-05 17:23:27 +0200

    Added docstrins and fixes for the pack & unpack functions

--- p2p_nse5/p2p/protocol.py
+++ p2p_nse5/p2p/protocol.py
@@ -3,6 +3,7 @@ import time
 import struct
 import hashlib
 import logging
+import dataclasses
 
 from Crypto.PublicKey import RSA
 from Crypto.Signature import pss
@@ -29,18 +30,11 @@ DEFAULT_PROOF_OF_WORK_BITS = 20  # TODO: Check which number of PoW bits is suffi
 HASH_ENDIAN = "big"
 
 
+@dataclasses.dataclass
 class ProtocolMessage:
     round_time: int
     proximity: int
     public_key: RSA.RsaKey
-
-    def __init__(self, round_time: int, proximity: int, public_key: RSA.RsaKey):
-        self.round_time = round_time
-        self.proximity = proximity
-        self.public_key = public_key
-
-    def __repr__(self):
-        return f"ProtocolMessage({', '.join(f'{k}={getattr(self, k)!r}' for k in dir(self) if not k.startswith('_'))})"
 
 
 def _check_proof_of_work(required_bits: int, body: bytes) -> bool:
@@ -60,34 +54,57 @@ def build_message(
     proof_of_work_bits = proof_of_work_bits or DEFAULT_PROOF_OF_WORK_BITS
     if not rsa_key.has_private():
         raise ValueError(f"RSA key {rsa_key} doesn't contain a private part!")
-
+    if rsa_key.size_in_bits() != 4096:
+        raise ValueError(f"Only RSA keys of size 4096 bits are supported")
     exported_public_key = rsa_key.public_key().export_key(format="DER")
     key_length = len(exported_public_key)
 
-    # Calculating a hash collision for the header and the public key in DEM format
+    # Calculate a hash collision for the header and the public key in DEM format (proof of work!)
     start = time.time()
     sample = -1
     hashed_header = None
-    for sample in range(1 << 64):
-        hashed_header = HASHED_HEADER.pack(key_length, proximity, round_time, sample) + exported_public_key
-        if _check_proof_of_work(proof_of_work_bits, hashed_header):
-            break
+    try:
+        for sample in range(1 << 64):
+            hashed_header = HASHED_HEADER.pack(key_length, proximity, round_time, sample) + exported_public_key
+            if _check_proof_of_work(proof_of_work_bits, hashed_header):
+                break
+    except struct.error as exc:
+        raise ValueError("Error packing the hashed header") from exc
     end = time.time()
 
-    # Signing the relevant payload and return it
+    # Sign the relevant payload and return it
     if hashed_header is None or sample == -1:
-        raise RuntimeError("Failed to calculate a hash collision. Invalid header configuration?")
+        raise ValueError("Failed to calculate a hash collision. Invalid header configuration?")
     if logger is not None:
-        logger.debug(f"Calculating message with {proof_of_work_bits}-bit hash collision took {end-start:.3f} seconds")
+        logger.debug(f"Calculating message with {proof_of_work_bits}-bit hash collision took {end - start:.3f} seconds")
     sig = pss.new(rsa_key).sign(SHA512.new(hashed_header))
-    return PROTOCOL_HEADER.pack(HEADER_CODE, hop_count, key_length, proximity, round_time, sample) + exported_public_key + sig
+    try:
+        header = PROTOCOL_HEADER.pack(HEADER_CODE, hop_count, key_length, proximity, round_time, sample)
+    except struct.error as exc:
+        raise ValueError("Error packing the protocol header") from exc
+    return header + exported_public_key + sig
 
 
-def unpack_message(msg: bytes, logger: logging.Logger = None, proof_of_work_bits: int = None) -> ProtocolMessage:
+def unpack_message(msg: bytes, min_proximity: int = None, proof_of_work_bits: int = None) -> ProtocolMessage:
+    """
+    Unpack an incoming message as raw bytes string into a `ProtocolMessage` instance
+
+    :param msg: raw bytes string that has been received as incoming NSE P2P message
+    :param min_proximity: minimal proximity; messages with a lower proximity to the
+        hash will be discarded before further checks of the content (e.g. signature
+        and proof of work) to improve this function's performance
+    :param proof_of_work_bits: optional indicator of the required bits of the proof of work hash
+    :return: the unpacked message as a ProtocolMessage instance
+    :raises ValueError: whenever something is wrong with the message, e.g. the
+        signature is invalid, the proof of work hash is invalid or not large
+        enough, the header has an invalid format or the message is too small
+    """
+
+    min_proximity = min_proximity or 0
     proof_of_work_bits = proof_of_work_bits or DEFAULT_PROOF_OF_WORK_BITS
     if not isinstance(msg, bytes):
         raise TypeError(f"Invalid message type {type(msg)}, expected bytes")
-    if len(msg) < 1024:
+    if len(msg) < 2 * SIGNATURE_LENGTH:
         raise ValueError("Message is too small, it can't hold all relevant data.")
 
     try:
@@ -100,6 +117,8 @@ def unpack_message(msg: bytes, logger: logging.Logger = None, proof_of_work_bits
         raise ValueError("Invalid hop count 0")
     if len(msg) != HEADER_LENGTH + pub_key_len + SIGNATURE_LENGTH:
         raise ValueError(f"Invalid public key length {pub_key_len} found in header")
+    if min_proximity > proximity:
+        raise ValueError(f"Message proximity {proximity} is smaller than minimum {min_proximity}")
 
     if not _check_proof_of_work(
             proof_of_work_bits,
@@ -118,10 +137,12 @@ def unpack_message(msg: bytes, logger: logging.Logger = None, proof_of_work_bits
 
 
 if __name__ == "__main__":
-    k = RSA.generate(4096)
+    import random
+
+    key = RSA.generate(4096)
     print("Generated 4096 bit RSA key.")
-    p = 42
-    t = 1337
+    p = random.randint(0, 64)
+    t = random.randint(0, 65536) * random.randint(0, 65536) * random.randint(0, 65536)
     m = build_message(key, p, t)
-    print(f"Message for proximity={p} and round time={t}:\n{m}")
+    print(f"Message for proximity={p} and round time={t} (len={len(m)}):\n{m}")
     print(f"Unpacked message: {unpack_message(m)}")
