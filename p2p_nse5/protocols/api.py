745f2f9c3d47fae9128960a84f826fe759b80b1e
Chris <chris@hopfen.space>
2022-07-10 03:10:48 +0200

    Implemented 'unpack_incoming_message' helper function

--- p2p_nse5/protocols/api.py
+++ p2p_nse5/protocols/api.py
@@ -1,5 +1,14 @@
 import dataclasses
-from typing import Any
+import struct
+from typing import Union
+
+from .msg_types import MessageType
+
+
+class InvalidMessage(Exception):
+    """
+    Exception for invalid API messages (they should simply be ignored)
+    """
 
 
 @dataclasses.dataclass
@@ -18,15 +27,7 @@ def pack_gossip_notify(data_type: int) -> bytes:
     pass  # TODO
 
 
-def unpack_gossip_notification(msg: bytes) -> GossipNotification:
-    pass  # TODO
-
-
 def pack_gossip_validation(message_id: int, valid: bool) -> bytes:
-    pass  # TODO
-
-
-def unpack_nse_query(msg: bytes) -> bool:
     pass  # TODO
 
 
@@ -34,15 +35,34 @@ def pack_nse_estimate(peers: int, std_deviation: int) -> bytes:
     pass  # TODO
 
 
-def unpack_incoming_message(msg: bytes) -> (int, Any):
+def unpack_incoming_message(msg: bytes) -> (int, Union[bool, GossipNotification]):
     """
     Parse, validate and unpack an incoming API message into type and data
 
     :param msg: incoming unchecked message in raw bytes
     :return: tuple of the message type identifier and the
         unpacked value specific to the specific message type
-    :raises ValueError: when an unsupported message type was
+    :raises InvalidMessage: when an unsupported message type was
         detected or when the message is malformed in any way
     """
 
-    pass  # TODO
+    if len(msg) < 4 or len(msg) > 65535:
+        raise InvalidMessage("Invalid length")
+    try:
+        size, msg_type = map(int, struct.Struct("!HH").unpack(msg[:4]))
+        if size != len(msg):
+            raise InvalidMessage("Invalid length field value")
+    except (ValueError, struct.error) as exc:
+        raise InvalidMessage("Invalid header start") from exc
+    if msg_type == MessageType.NSE_QUERY:
+        return msg_type, True
+    elif msg_type == MessageType.GOSSIP_NOTIFICATION:
+        if size < 8:
+            raise InvalidMessage("Message too short")
+        try:
+            msg_id, data_type = map(int, struct.Struct("!HH").unpack(msg[4:8]))
+        except (ValueError, struct.error) as exc:
+            raise InvalidMessage("Invalid gossip notification message") from exc
+        return msg_type, GossipNotification(size=size, message_id=msg_id, data_type=data_type, data=msg[8:])
+    else:
+        raise InvalidMessage(f"Unknown incoming message type: {msg_type}")
