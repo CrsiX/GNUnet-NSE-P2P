e9fbaeb7d5ef0faea107fe71340cac9839e3ac38
Chris <chris@hopfen.space>
2022-08-16 16:03:55 +0200

    Updated the default config, added a check that it works with it, too

--- tests/cli.py
+++ tests/cli.py
@@ -1,5 +1,8 @@
 import sys
+import time
+import socket
 import unittest
+import threading
 import subprocess
 
 
@@ -11,3 +14,18 @@ class CLITests(unittest.TestCase):
         self.assertEqual(p.returncode, 0)
         self.assertIn("Network Size Estimation (NSE)", p.stdout.decode("UTF-8"))
         self.assertIn("Licensed under AGPLv3.", p.stdout.decode("UTF-8"))
+
+    def test_app_runs_with_defaults(self):
+        def _run_dummy_tcp_server(host, port, delay):
+            server = socket.create_server((host, port))
+            s, a = server.accept()
+            time.sleep(delay)
+            s.close()
+            server.close()
+
+        t = threading.Thread(target=_run_dummy_tcp_server, args=("localhost", 5000, 0.5))
+        t.start()
+        default = "p2p_nse5/static/default_configuration.ini"
+        with self.assertRaises(subprocess.TimeoutExpired):
+            subprocess.run([sys.executable, "-m", "p2p_nse5", "-c", default, "run"], timeout=0.5, capture_output=False)
+        t.join()
