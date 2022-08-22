25b464f148e90e2051ca5b70ca67e771a79aac8f
Chris <chris@hopfen.space>
2022-08-22 17:12:58 +0200

    Fixed the runnable test with increased timeouts for slow machines

--- tests/cli.py
+++ tests/cli.py
@@ -8,12 +8,12 @@ import subprocess
 
 class CLITests(unittest.TestCase):
     def test_is_app_runnable(self):
-        p = subprocess.run([sys.executable, "-m", "p2p_nse5"], timeout=0.5, capture_output=True)
+        p = subprocess.run([sys.executable, "-m", "p2p_nse5"], timeout=1.0, capture_output=True)
         self.assertEqual(p.returncode, 2)
-        p = subprocess.run([sys.executable, "-m", "p2p_nse5", "-h"], timeout=0.5, capture_output=True)
+        p = subprocess.run([sys.executable, "-m", "p2p_nse5", "-h"], timeout=1.0, capture_output=True)
         self.assertEqual(p.returncode, 0)
-        self.assertIn("Network Size Estimation (NSE)", p.stdout.decode("UTF-8"))
-        self.assertIn("Licensed under AGPLv3.", p.stdout.decode("UTF-8"))
+        self.assertIn(b"Network Size Estimation (NSE)", p.stdout)
+        self.assertIn(b"Licensed under AGPLv3.", p.stdout)
 
     # TODO: The defaults are usually not good enough to use directly and need adoption.
     #  Therefore, better add a bunch of unittests with specific configurations.
