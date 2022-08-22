eef2f3e1772113e47723274731752412b10a32d4
Chris <chris@hopfen.space>
2022-08-22 16:53:10 +0200

    Prepared to move the NSE helpers to the protocols submodule

---
+++ p2p_nse5/protocols/__init__.py
@@ -0,0 +1,14 @@
+"""
+Package containing helper functionality to make use of
+the three different protocols used in this project:
+
+  * Module :mod:`protocols.api <p2p_nse5.protocols.api>` is responsible
+    for interacting with the other modules of the VoidPhone P2P application
+    (e.g. the Gossip module as server or the RPS module as client)
+  * Module :mod:`protocols.nse <p2p_nse5.protocols.nse>` contains a
+    collection of convenience features that are used by the NSE scheduler
+    and the respective handlers (it doesn't directly contain a protocol)
+  * Module :mod:`protocols.p2p <p2p_nse5.protocols.p2p>` is responsible
+    for handling the protocol messages which are delivered by Gossip
+    and are used to spread information about the network size
+"""
